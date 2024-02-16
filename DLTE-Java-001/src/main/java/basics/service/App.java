7ec192cb8bb5e8fd89d178be43b9bad8f931d9e5
razzaksr <razzaksr@gmail.com>
2024-02-16 12:33:20 +0530

    java maven project

---
+++ DLTE-Java-001/src/main/java/basics/service/App.java
@@ -0,0 +1,13 @@
+package basics.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
