26dc83532f9bd1576bbc6813cead9d92d60f0bf1
razzaksr <razzaksr@gmail.com>
2024-03-27 17:47:01 +0530

    Spring boot Security level3

--- DLTE-Spring-Framework/DLTE-Spring-007-SOAP/src/main/java/elements/spring/explore/security/LoanSecurity.java
+++ DLTE-Spring-Framework/DLTE-Spring-007-SOAP/src/main/java/elements/spring/explore/security/LoanSecurity.java
@@ -1,10 +1,15 @@
 package elements.spring.explore.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,20 +20,41 @@ import java.util.stream.Stream;
 @Configuration
 public class LoanSecurity {
 
+    @Autowired
+    private MyBankOfficialsService service;
+
+    AuthenticationManager manager;
+
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        UserDetails userDetails1= User.withDefaultPasswordEncoder().username("elroy").password("monis").roles("admin").build();
-        UserDetails userDetails2= User.withDefaultPasswordEncoder().username("akshira").password("hegde").roles("manager").build();
-        List<UserDetails> userDetailsList=Stream.of(userDetails1,userDetails2).collect(Collectors.toList());
-        return new InMemoryUserDetailsManager(userDetailsList);
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        UserDetails userDetails1= User.withDefaultPasswordEncoder().username("elroy").password("monis").roles("admin").build();
+//        UserDetails userDetails2= User.withDefaultPasswordEncoder().username("akshira").password("hegde").roles("manager").build();
+//        List<UserDetails> userDetailsList=Stream.of(userDetails1,userDetails2).collect(Collectors.toList());
+//        return new InMemoryUserDetailsManager(userDetailsList);
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().anyRequest().authenticated();
         httpSecurity.csrf().disable();
         httpSecurity.httpBasic();
         httpSecurity.formLogin();
+
+        httpSecurity.authorizeRequests().antMatchers("/profile/register").permitAll();
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+
+
+        // 3rd layer
+        AuthenticationManagerBuilder builder=httpSecurity.
+                getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(service);
+        manager=builder.build();
+        httpSecurity.authenticationManager(manager);
+
         return httpSecurity.build();
     }
 }
---
+++ DLTE-Spring-Framework/DLTE-Spring-007-SOAP/src/main/java/elements/spring/explore/security/MyBankOfficials.java
@@ -0,0 +1,70 @@
+package elements.spring.explore.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyBankOfficials implements UserDetails {
+    private String fullName;
+    private String username;
+    private String password;
+    private long contact;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+}
