47d975b38e9728ede8088a17099e0f0998a41edb
razzaksr <razzaksr@gmail.com>
2024-03-27 16:39:29 +0530

    Spring boot Security level2

--- DLTE-Spring-Framework/DLTE-Spring-007-SOAP/src/main/java/elements/spring/explore/security/LoanSecurity.java
+++ DLTE-Spring-Framework/DLTE-Spring-007-SOAP/src/main/java/elements/spring/explore/security/LoanSecurity.java
@@ -3,10 +3,26 @@ package elements.spring.explore.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class LoanSecurity {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails userDetails1= User.withDefaultPasswordEncoder().username("elroy").password("monis").roles("admin").build();
+        UserDetails userDetails2= User.withDefaultPasswordEncoder().username("akshira").password("hegde").roles("manager").build();
+        List<UserDetails> userDetailsList=Stream.of(userDetails1,userDetails2).collect(Collectors.toList());
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().anyRequest().authenticated();
