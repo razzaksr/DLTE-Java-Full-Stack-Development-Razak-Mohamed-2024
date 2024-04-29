d9011f594bc829cab73425d500e85f0486c3012c
razzaksr <razzaksr@gmail.com>
2024-04-29 17:16:24 +0530

    dao's application and service's apps properties both will work

--- DLTE-Spring-Framework/service/service/src/main/java/layers/backend/service/ServiceApplication.java
+++ DLTE-Spring-Framework/service/service/src/main/java/layers/backend/service/ServiceApplication.java
@@ -2,8 +2,13 @@ package layers.backend.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySource("classpath:apps.properties")
 public class ServiceApplication {
 
 	public static void main(String[] args) {
